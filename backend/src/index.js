2e0d8eaf99ab10ad5356361b95d82f36a28a53d5
sibaram <sibaramnahak12345@gmail.com>
2026-05-27 15:58:40 +0530

    updating the code.

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,14 +5,32 @@ import todosRouter from "./routes/todos.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
+// const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
 
-app.use(
-  cors({
-    origin: CORS_ORIGIN,
-  })
-);
+// app.use(
+//   cors({
+//     origin: CORS_ORIGIN,
+//   })
+// );
 app.use(express.json());
+
+const allowedOrigins = process.env.CORS_ORIGINS
+  ? process.env.CORS_ORIGINS.split(",")
+  : [];
+
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+  
+        if (allowedOrigins.includes(origin)) {
+          return callback(null, true);
+        }
+  
+        return callback(new Error("Not allowed by CORS"));
+      },
+    })
+  );
 
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
